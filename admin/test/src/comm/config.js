a63a17b70181c9b5b9e1fe1f4d82b8980b3396ad
weiwei <526710585@qq.com>
2020-07-04 10:33:32 +0800

    1.通用的CRUD接口

--- admin/test/src/comm/config.js
+++ admin/test/src/comm/config.js
@@ -1,5 +1,3 @@
 export const host = 'http://localhost:3001' + '/admin/api'
 //get 获取所有分类  post新建分类
-export const categoryUrl = host + '/categories'
-//post 查找分类
-export const findCategoryUrl = host + '/findcategory'
+export const categoryUrl = host + '/rest/categories'
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,41 +1,60 @@
 module.exports = app => {
   const express = require('express')
-  const router = express.Router()
+  const router = express.Router({
+    mergeParams: true, //吧app.use中的params参数传递到路由中 不然拿不到路由
+  })
+
+  const inflection = require('inflection'); //用来转换类名的插件  吧小写的接口转换为model的大写名称
 
   //拿到模型
-  const Category = require('../../models/Category')
+  // const Category = require('../../models/Category')
 
-  console.log(Category);
-  router.post('/categories', async (req, res) => {
-    const model = await Category.create(req.body)
+
+  router.post('/', async (req, res) => {
+    const model = await req.Model.create(req.body)
     console.log('req.body', req.body);
     res.send(model)
   })
-  router.get('/categories', async (req, res) => {
+
+  router.get('/', async (req, res) => {
+    const ModelName = req.Model.modelName;
+    const queryOptions = {}
+    if (ModelName === 'Category') { //匹配只有查询Category模型时 才填充parent选项
+      queryOptions.populate = 'parent'
+    }
     //populate 关联查询:不仅仅是要parent这个参数的id 而是要查出整个数据作为一个对象
-    const model = await Category.find().populate('parent')
+    const model = await req.Model.find().setOptions(queryOptions).limit(10)
     res.send(model)
   })
-  router.get('/categories/:id', async (req, res) => {
-    const model = await Category.findById(req.params.id)
+
+  router.get('/:id', async (req, res) => {
+    const model = await req.Model.findById(req.params.id)
     res.send(model)
   })
-  router.delete('/categories/:id', async (req, res) => {
+
+  router.delete('/:id', async (req, res) => {
     console.log(req.params.id);
-    const model = await Category.findByIdAndDelete(req.params.id)
+    const model = await req.Model.findByIdAndDelete(req.params.id)
     res.send({
       result: 0
     })
   })
-  router.put('/categories/:id', async (req, res) => {
-    // const model = await Category.findById(req.params.id)
+
+  router.put('/:id', async (req, res) => {
+    // const model = await req.Model.findById(req.params.id)
     // model.name = req.body.name;
     // model.parent = req.body.parent;
     // await model.save();
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body) //一句话代替上面的所有代码
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body) //一句话代替上面的所有代码
     res.send({
       result: 0
     })
   })
-  app.use('/admin/api', router)
+
+  app.use('/admin/api/rest/:resource', (req, res, next) => {
+    const modelName = inflection.classify(req.params.resource) //使用inflection转换名称
+    const Model = require(`../../models/${modelName}`)
+    req.Model = Model;
+    next();
+  }, router)
 }
